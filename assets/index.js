c2c7672162bfa11c8a0fff1c18dab51819c11f13
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-23 15:31:11 -0300

    feat: finalizado projeto

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+const section = document.getElementById("img");
+const cards = document.querySelectorAll(".cards");
+const avancar = document.getElementById("avancar");
+const voltar = document.getElementById("voltar");
+
+let avancarControle = 0;
+
+function carroselAvancar() {
+  avancarControle++;
+
+  if (avancarControle > cards.length - 1) {
+    avancarControle = 0;
+  }
+
+  section.style.transform = `translateX(${-avancarControle * 380}px)`;
+}
+
+function carroselVoltar() {
+  avancarControle--;
+  if (avancarControle < 0) {
+    avancarControle = cards.length - 1;
+  }
+
+  section.style.transform = `translateX(${-avancarControle * 380}px)`;
+}
+
+avancar.addEventListener("click", carroselAvancar);
+voltar.addEventListener("click", carroselVoltar);
